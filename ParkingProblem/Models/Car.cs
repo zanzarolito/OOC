75f404d2f7c389e305ba147b9ddafd6ff448b7f2
Anjara Rabesandratana <anjara.rabesandratana@epita.fr>
2021-02-22 15:06:41 +0000

    Anjara Rabesandratana - OOP Parking Problem

---
+++ ParkingProblem/Models/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingProblem.Models
+{
+    class Car
+    {
+        public int hours { get; set; }
+
+
+        public Car(int _hours)
+        {
+            hours = _hours;
+        }
+
+        public double Charges()
+        {
+            if (hours >= 24)
+                return 10;
+            if (hours <= 3)
+                return 2;
+            else
+                return 2 + (hours - 3) * 0.5;
+        }
+
+
+    }
+}
---
+++ ParkingProblem/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ParkingProblem.Models;
+using System.Collections.Generic;
+
+namespace ParkingProblem
+{
+    class Program
+    {
+        static double TotalCharges(List<Car> Garage)
+        {
+            double Receipt = 0;
+            int i = 0;
+
+            while(i < Garage.Count)
+            {
+                Receipt += Garage[i].Charges();
+                i++;
+            }
+                 
+
+            return Receipt;
+        }
+        static void Main(string[] args)
+        {
+            double Price = 0;
+
+            
+            
+            ;
+
+            List<Car> Garage = new List<Car>();
+
+
+            Car Lambo = new Car(5);
+            Car Ferrari = new Car(10);
+            Car Ford = new Car(2);
+            Car Fiat = new Car(7);
+
+
+            Garage.Add(Lambo);
+            Garage.Add(Ferrari);
+            Garage.Add(Ford);
+            Garage.Add(Fiat);
+
+            double receipt = TotalCharges(Garage);
+
+
+            Console.WriteLine("Receipt of the day : {0} euro(s)", receipt);
+        }
+    }
+}
